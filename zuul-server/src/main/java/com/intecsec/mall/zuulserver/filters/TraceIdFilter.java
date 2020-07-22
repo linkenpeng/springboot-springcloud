e254fa36e67d72d47411211f97ec7ff029f6f74c
Peter.Peng <peter.peng@watsons.com.cn>
2020-07-22 11:26:55 +0800

    add sleuth and support logback

--- zuul-server/src/main/java/com/intecsec/mall/zuulserver/filters/TraceIdFilter.java
+++ zuul-server/src/main/java/com/intecsec/mall/zuulserver/filters/TraceIdFilter.java
@@ -13,18 +13,19 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TraceIdFilter extends ZuulFilter {
+	private static final int FILTER_ORDER = 2;
 
 	@Autowired
 	private Tracer tracer;
 
 	@Override
 	public String filterType() {
-		return "post";
+		return FilterUtils.POST_FILTER_TYPE;
 	}
 
 	@Override
 	public int filterOrder() {
-		return 1;
+		return FILTER_ORDER;
 	}
 
 	@Override
@@ -35,7 +36,7 @@ public class TraceIdFilter extends ZuulFilter {
 	@Override
 	public Object run() throws ZuulException {
 		RequestContext ctx = RequestContext.getCurrentContext();
-		ctx.getResponse().addHeader("scd-trace-id", tracer.currentSpan().toString());
+		ctx.getResponse().addHeader("scd-trace-id", tracer.currentSpan().context().traceIdString());
 		return null;
 	}
 }
